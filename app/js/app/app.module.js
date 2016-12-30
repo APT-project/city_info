70aa6a72bb93735f24d12d1c800fc89b9a49c4b2
Jara <jsaarinen92@gmail.com>
2016-12-30 14:24:38 +0200

    Copy tabcode from Junyi's PR

--- app/js/app/app.module.js
+++ app/js/app/app.module.js
@@ -35,7 +35,7 @@
     llb_app.request('window_dimensions');
   };
 
-  angular.module('cityInfo', ['ngMaterial', 'cityInfo.map', 'cityInfo.poi'])
+  angular.module('cityInfo', ['ngMaterial', 'cityInfo.map', 'cityInfo.poi', 'cityInfo.news'])
   .config(['$mdThemingProvider', 'GoogleMapsApiProvider', configureApp])
   .run(['$rootScope', onAppInitialized]);
 })();
