a1781cfbf80550e8d0872b23725820b3ee57be62
Jara <jsaarinen92@gmail.com>
2016-12-30 14:33:34 +0200

    Move tab variable explicitly to rootscope

--- app/js/app/app.module.js
+++ app/js/app/app.module.js
@@ -12,6 +12,7 @@
   function onAppInitialized($rootScope) {
     $rootScope.fullscreen = false;
     $rootScope.initialized = false;
+    $rootScope.tab = 1;
 
     llb_app.addListener('window_state', function(data) {
       $rootScope.$apply(function() {
